ab3fbe6ec449d06e832b8315e09861236db21bf2
nfabina <mani.fabina@gmail.com>
2018-02-03 18:01:25 -0500

    Delete plot3.R

--- Project 1/plot3.R
+++
@@ -1,9 +0,0 @@
-with(filter, {
-  plot(Sub_metering_1 ~ dateTime, type = "l", 
-       ylab = "Energy sub metering", xlab = "")
-  lines(Sub_metering_2 ~ dateTime, col = 'Red')
-  lines(Sub_metering_3 ~ dateTime, col = 'Blue')
-})
-legend("topright", col = c("black", "red", "blue"), lwd = c(2,2,2), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")) 
-dev.copy(png,"plot3.png", width=480, height=480)
-dev.off()
